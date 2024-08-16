b95f1d4b03d7f55b2a8037450ee61c46ed876664
Hun <dudgns9194@gmail.com>
2024-08-17 02:06:26 +0900

    Init: 데이터베이스 연결
    mysql 데이터베이스 연결, env파일 설정

---
+++ server/config/db.js
@@ -0,0 +1,18 @@
+const mysql = require('mysql2/promise');
+
+const db = {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE
+};
+
+const query = async (sql, values = undefined) => {
+    const conn = await mysql.createConnection(db);
+    
+    const [rows, fields] = values ? await conn.query(sql, values) : await conn.query(sql);
+    return rows;
+};
+
+module.exports = query;
