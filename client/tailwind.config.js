21ff8c4d639a897d1c28a469f18d1196dfcaf93e
hwinareun <hwinareun@gmail.com>
2024-08-21 05:04:08 +0900

    FE-design: tailwind 주요 색상 정의 #22

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,8 +2,16 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        medicineNeutral: '#F5F8FF',
+        medicinePrimary: '#E6EDFC',
+        medicineSecondary: '#C2D6FF',
+        medicinePositive: '#94C2FF',
+        medicinePoint: '#0000E3',
+        medicineNegative: '#ECECEC',
+      },
+    },
   },
   plugins: [],
 };
-
