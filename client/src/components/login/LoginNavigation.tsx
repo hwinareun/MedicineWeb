9cf8ee01856e9c55c4c1c25072f1bfb57e770742
nulzi-laptop <kimty131@naver.com>
2024-08-20 17:44:50 +0900

    Feat: add component LoginNavigation
    - 회원가입 및 아이디 / 비밀번호 찾기 네비게이션 컴포넌트 추가
    - 추후 이동 url은 수정될 수 있음

---
+++ client/src/components/login/LoginNavigation.tsx
@@ -0,0 +1,12 @@
+export const LoginNavigation = () => {
+  return (
+    <div className="flex w-full justify-between mt-2 text-xs">
+      <a className="underline" href="/join">
+        회원가입
+      </a>
+      <a className="underline" href="/find">
+        아이디 / 비밀번호 찾기
+      </a>
+    </div>
+  );
+};
