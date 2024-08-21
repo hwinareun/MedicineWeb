076a6a3e67cd48c96d2ef02ec7e447ff50713b28
hwinareun <hwinareun@gmail.com>
2024-08-22 08:17:38 +0900

    FE-Style: 상세 조회 모달 CSS 구성
    -상세 조회 모달 CSS 기본 구성.
    - 공통 헤더 및 푸터 margin 삭제.
    
    #37

--- client/src/components/common/Footer.tsx
+++ client/src/components/common/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <div className="flex flex-row items-center justify-end w-auto px-2 mt-20 text-sm bg-blue-100 h-14">
+    <div className="flex flex-row items-center justify-end w-auto px-2 text-sm bg-blue-100 h-14">
       Team.medicineWeb
     </div>
   );
--- client/src/pages/Search.tsx
+++ client/src/pages/Search.tsx
@@ -4,17 +4,23 @@ import SearchBox from '../components/search/SearchBox';
 import SearchFilter from '../components/search/SearchFilter';
 import { DrugData } from '../types/drug.type';
 import ReferenceEmpty from '../components/reference/ReferenceEmpty';
+import ReferenceDetail from '../components/reference/ReferenceDetail';
 
 const Search = () => {
   const [results, setResults] = useState<DrugData[]>([]);
 
   return (
-    <div className="flex p-4 m-4">
-      <div className="flex flex-col p-4 m-4 bg-medicineNeutral w-fit h-fit whitespace-nowrap">
-        <SearchBox setResults={setResults} />
-        <SearchFilter setResults={setResults} />
+    <div className="flex flex-col p-4 m-4">
+      <div className="flex">
+        <div className="flex flex-col p-4 m-4 bg-medicineNeutral w-fit h-fit whitespace-nowrap">
+          <SearchBox setResults={setResults} />
+          <SearchFilter setResults={setResults} />
+        </div>
+        {results.length > 0 ? <Reference data={results} /> : <ReferenceEmpty />}
       </div>
-      {results.length > 0 ? <Reference data={results} /> : <ReferenceEmpty />}
+      <div className="absolute flex items-center justify-center w-svw h-svh">
+        <ReferenceDetail />
+      </div>
     </div>
   );
 };
