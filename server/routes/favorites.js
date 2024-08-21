42f872c515ffbb80eb466907851ca73bf6b6faea
Hun <dudgns9194@gmail.com>
2024-08-22 02:26:40 +0900

    Feat: Add add favorite
    즐겨찾기추가 기능 구현

--- server/routes/favorites.js
+++ server/routes/favorites.js
@@ -5,9 +5,10 @@ const {
   addFavorite,
   removeFavorite,
 } = require("../controllers/favoriteController");
+const { authenticateJWT } = require('../middlewares/auth');
 
 router.get("/", getFavorites);
-router.post("/:drugId", addFavorite);
+router.post("/:drugId", authenticateJWT, addFavorite);
 router.delete("/:drugId", removeFavorite);
 
 module.exports = router;
