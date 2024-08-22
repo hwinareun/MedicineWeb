69060128d9dbc67ece4a5aacfb5cff84205c1908
Hun <dudgns9194@gmail.com>
2024-08-23 00:14:50 +0900

    Fix: myprofile error
    즐겨찾기를 하지 않았을 시 에러 발생 문제 해결

--- server/utils/getCommonByKey.js
+++ server/utils/getCommonByKey.js
@@ -1,16 +1,11 @@
 const removeDuplicateObjects = require("./removeDuplicateObjects");
 
 const getCommonByKey = async (arr1, arr2, key = 'itemSeq') => {
-
     const filteredArr1 = await arr1.filter(item1 => arr2.some(item2 => item1[key] === item2[key]));
     const filteredArr2 = await arr2.filter(item1 => arr1.some(item2 => item1[key] === item2[key]));
-    // const filteredArr3 = new Set(await arr3.filter(item1 => filteredArr1.some(item2 => item1[key] === item2[key])));
-
-    // const filteredArr3_1 = await removeDuplicateObjects(filteredArr3);
 
     const ArrayArr1 = await filteredArr1.map(obj => Object.values(obj));
     const ArrayArr2 = await filteredArr2.map(obj => Object.values(obj));
-    // const ArrayArr3 = await filteredArr3_1.map(obj => Object.values(obj));
 
     return [filteredArr1, filteredArr2, ArrayArr1, ArrayArr2];
 }
