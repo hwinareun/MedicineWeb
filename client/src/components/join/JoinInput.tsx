1f31899946ecd1115f1beca6e55d0ec1524f8ac3
nulzi-laptop <kimty131@naver.com>
2024-08-23 16:04:53 +0900

    Feat: add 아이디 찾기, 비밀번호 초기화
    - 아이디 찾기 기능 추가
    - 비밀번호 초기화 요청 기능 추가
    - 비밀번호 초기화 기능 추가

--- client/src/components/join/JoinInput.tsx
+++ client/src/components/join/JoinInput.tsx
@@ -3,6 +3,7 @@ interface Props {
   type?: string;
   value: string;
   placeholder?: string;
+  disabled?: boolean;
   onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
@@ -10,21 +11,23 @@ export const JoinInput: React.FC<Props> = ({
   label,
   type,
   value,
+  disabled,
   placeholder,
   onChange,
 }) => {
   return (
     <>
-      <label className="mr-1 flex-1 text-right pb-1" htmlFor="">
+      <label className="mr-1 flex-1 text-right text-xl" htmlFor="">
         {label}:
       </label>
       <input
+        disabled={disabled}
         required
         type={type ?? 'text'}
         value={value}
         placeholder={placeholder}
         onChange={onChange}
-        className="border border-blue-400 focus:outline-blue-800 placeholder:text-black-400 w-60 h-9 placeholder:text-ts rounded p-1"
+        className="border border-blue-400 focus:outline-blue-800 placeholder:text-black-400 w-96 h-12 placeholder:text-lg rounded p-2"
       />
     </>
   );
--- client/src/pages/FindUser.tsx
+++ client/src/pages/FindUser.tsx
@@ -1,6 +1,84 @@
+import { useState } from 'react';
+import { FindUserBox } from '../components/findUser/FindUserBox';
+import { findId, reqResetPassword, resetPassword } from '../apis/users.api';
+import { useNavigate } from 'react-router-dom';
+
 const FindUser = () => {
+  const [warnTextId, setWarnTextId] = useState('');
+  const [warnTextPw, setWarnTextPw] = useState('');
+  const [showResetPw, setShowResetPw] = useState(false);
+  const navigate = useNavigate();
+
+  const onSubmitId = (e: React.SyntheticEvent) => {
+    e.preventDefault();
+    // type에 대한 에러가 발생했지만 동작에는 문제가 없다.
+    // 나중에 수정 예정
+    const nickname = e.target[0].value;
+    const question = e.target[1].value;
+    const answer = e.target[2].value;
+
+    findId({ nickname, question, answer })
+      .then((res) => {
+        setWarnTextId(`성공! 아이디는 '${res.id}' 입니다`);
+      })
+      .catch((err) => {
+        console.log(err);
+        setWarnTextId('아이디를 찾을 수 없습니다');
+      });
+  };
+  const onSubmitPw = (e: React.SyntheticEvent) => {
+    e.preventDefault();
+    const id = e.target[0].value;
+    const question = e.target[1].value;
+    const answer = e.target[2].value;
+    const password = e.target[3].value;
+
+    if (showResetPw) {
+      resetPassword({ id, password })
+        .then((res) => {
+          if (
+            confirm('비밀번호 변경에 성공했습니다. 로그인하러 이동하겠습니까?')
+          ) {
+            // 로그인 화면 이동
+            navigate('/login');
+          } else {
+            // 로그인 화면으로 이동하지 않는 상황
+            setShowResetPw(false);
+            setWarnTextPw('');
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+          setWarnTextPw('비밀번호 변경에 실패했습니다');
+        });
+    } else {
+      reqResetPassword({ id, question, answer })
+        .then((res) => {
+          // 리셋 요청 성공 시 비밀번호 input 보이기
+          setShowResetPw(true);
+          setWarnTextPw('비밀번호를 초기화 요청에 성공했습니다');
+        })
+        .catch((err) => {
+          console.log(err);
+          setWarnTextPw('질문에 대한 답이 잘못되었습니다');
+        });
+    }
+  };
+
   return (
-    <div className="flex-grow flex items-center justify-center">FindUser</div>
+    <div className="flex-grow flex flex-col items-center justify-evenly">
+      <FindUserBox
+        title="아이디 찾기"
+        warnText={warnTextId}
+        onSubmit={onSubmitId}
+      />
+      <FindUserBox
+        title="비밀번호 초기화"
+        warnText={warnTextPw}
+        onSubmit={onSubmitPw}
+        showResetPw={showResetPw}
+      />
+    </div>
   );
 };
 
