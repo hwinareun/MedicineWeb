8641b3a76b73e1c1d5a23054b4608ca6ff142b36
nulzi-laptop <kimty131@naver.com>
2024-08-23 18:53:13 +0900

    Design: font size up
    font size up

--- client/src/components/join/JoinDupCheckButton.tsx
+++ client/src/components/join/JoinDupCheckButton.tsx
@@ -5,7 +5,7 @@ interface Props {
 export const JoinDupCheckButton: React.FC<Props> = ({ onClick }) => {
   return (
     <button
-      className="absolute right-[-70px] border text-xs ml-2 border-blue-400 bg-blue-200 w-16 h-9 rounded-lg hover:bg-blue-400 hover:text-white"
+      className="absolute right-[-70px] border text-sm ml-2 border-blue-400 bg-blue-200 w-16 h-9 rounded-lg hover:bg-blue-400 hover:text-white"
       type="button"
       onClick={onClick}
     >
