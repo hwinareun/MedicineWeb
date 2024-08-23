0cb2e47c96a8f157383540a8d648fc136b8b88fd
nulzi-laptop <kimty131@naver.com>
2024-08-24 00:50:50 +0900

    Fix: token을 저장하는 타이밍 수정
    token을 저장하고 사용하는 타이밍이 틀렸기 때문에 interceptor 사용

--- client/src/apis/http.ts
+++ client/src/apis/http.ts
@@ -11,3 +11,16 @@ export const httpClient = axios.create({
   },
   withCredentials: true,
 });
+
+httpClient.interceptors.request.use(
+  (config) => {
+    const token = getToken();
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
