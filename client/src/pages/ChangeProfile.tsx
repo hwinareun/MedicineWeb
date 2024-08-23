478c98be2665a4cf7e9cd6b596a2bb5456b4285c
nulzi-laptop <kimty131@naver.com>
2024-08-24 00:57:47 +0900

    Fix: 비밀번호 확인 기능 on
    비밀번호 확인 기능 on

--- client/src/pages/ChangeProfile.tsx
+++ client/src/pages/ChangeProfile.tsx
@@ -3,7 +3,7 @@ import { PwCheck } from '../components/changeProfile/PwCheck';
 import { ChangeProfileBox } from '../components/changeProfile/ChangeProfileBox';
 
 const ChangeProfile = () => {
-  const [isChecked, setIsChecked] = useState(true);
+  const [isChecked, setIsChecked] = useState(false);
 
   return (
     <div className="flex-grow flex items-center justify-center">
