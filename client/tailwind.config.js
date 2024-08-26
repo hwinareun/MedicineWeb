7c45a8753107bcf10aff2ad567919273656fcba3
nulzi-laptop <kimty131@naver.com>
2024-08-26 14:55:21 +0900

    Design: ReferenceDetail 스크롤바 삭제
    tailwind plugin tailwind-scollbar-hide를 사용해 삭제

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+import scrollbarHide from 'tailwind-scrollbar-hide';
+
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
@@ -14,5 +16,5 @@ export default {
       },
     },
   },
-  plugins: [],
+  plugins: [scrollbarHide],
 };
