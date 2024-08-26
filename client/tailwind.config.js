890ba7180ab00af848e8f432001cf602684092a9
nulzi-laptop <kimty131@naver.com>
2024-08-26 18:27:29 +0900

    Design: main navigator 이미지 등장 애니메이션 추가
    main navigator 이미지 등장 애니메이션 추가

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -12,6 +12,34 @@ export default {
         medicineNegative: '#ECECEC',
         medicineFontBlue: '#60A5FA',
       },
+      keyframes: {
+        crawlOut1: {
+          '0%': { transform: 'translate(0px,0px)', opacity: '0' },
+          '100%': {
+            transform: 'translate(0px,-27px)',
+            opacity: '1',
+          },
+        },
+        crawlOut2: {
+          '0%': { transform: 'translate(0px,0px)', opacity: '0' },
+          '100%': {
+            transform: 'translate(-15px,-10px) rotate(-40deg)',
+            opacity: '1',
+          },
+        },
+        crawlOut3: {
+          '0%': { transform: 'translate(0px,0px)', opacity: '0' },
+          '100%': {
+            transform: 'translate(15px,-10px) rotate(40deg)',
+            opacity: '1',
+          },
+        },
+      },
+      animation: {
+        crawlOut1: 'crawlOut1 0.3s forwards',
+        crawlOut2: 'crawlOut2 0.3s forwards',
+        crawlOut3: 'crawlOut3 0.3s forwards',
+      },
     },
   },
   plugins: [],
